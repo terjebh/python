459fa7ad7c87ead4d1e183c3f11252e21b16814b
Terje Berg-Hansen <terje@itfakultetet.no>
2022-05-14 13:20:44 +0200

    randint()

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 import numpy as np
 import scipy as sc
+from matplotlib import pyplot as plt
 
 # print("Hei") 
 
-for i in range(1,11):
+for i in range(1,3):
     print("tall "+ str(i) + ": " + str(np.random.rand()))
+   
+for i in range(1,7):
+    print(f"Terningkast {i}: {np.random.randint(1,7)}") 
+ 
 
+# plt.plot(np.random.randn(15,20))
+# plt.show()
 
